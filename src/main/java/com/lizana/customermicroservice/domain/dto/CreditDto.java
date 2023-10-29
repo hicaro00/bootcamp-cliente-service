2397cedb437a6a9ace0a2afcf33303fc7294ae0c
humberto <humberto.lizana@gmail.com>
2023-10-29 11:39:55 -0500

    fix

---
+++ src/main/java/com/lizana/customermicroservice/domain/dto/CreditDto.java
@@ -0,0 +1,52 @@
+package com.lizana.customermicroservice.domain.dto;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.List;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class CreditDto {
+  private String creditId; // identificador unico de credito
+  private String customerId; //id del clietne asociado al credito
+  private String creditCardId; //identificadort de la tarjeta asociada al credito
+  private String bankAccountId; //id de la cuenta bancaria asociada si la hubiera
+  private String creditType; //el tipo de credito  PERSONAL ||EMPRESARIAL
+  private LocalDateTime date;  //fecha en que se crea o aprueva el credito
+  private String interestRate;  // tasa de interes del credito aprovado
+  private BigDecimal totalAmount;  // monto total del credito aprobado
+  private BigDecimal totalInterest; // interes totales generados por el credito
+  private BigDecimal outstandingBalance; //saldo pendietne as pagar
+  private DepositAmountDto depositAmountDto; //deposito o pago de deuda
+  private WithdrawalAmountDto withdrawalAmountDto; //retiro de saldo del credito
+
+
+  public static CreditDto combineAll(List<CreditDto> creditDtos) {
+    if (creditDtos == null || creditDtos.isEmpty()) {
+      return new CreditDto(); // Devuelve un CreditDto vacío si la lista está vacía o nula
+    }
+
+
+    CreditDto combinedCreditDto = new CreditDto();
+
+    combinedCreditDto.setCustomerId(creditDtos.get(0).getCustomerId());
+
+    for (CreditDto creditDto : creditDtos) {
+      combinedCreditDto.setCustomerId(creditDto.getCustomerId());
+      combinedCreditDto.setInterestRate(combinedCreditDto.getInterestRate());
+      combinedCreditDto.setCreditType(combinedCreditDto.getCreditType());
+      combinedCreditDto.setDate(combinedCreditDto.getDate());
+      combinedCreditDto.setTotalAmount(combinedCreditDto.getTotalAmount().add(creditDto.getTotalAmount()));
+      combinedCreditDto.setTotalInterest(combinedCreditDto.getTotalInterest().add(creditDto.getTotalInterest()));
+      combinedCreditDto.setOutstandingBalance(combinedCreditDto.getOutstandingBalance());
+
+    }
+
+    return combinedCreditDto;
+  }
+
+}
---
+++ src/main/java/com/lizana/customermicroservice/infrastructure/outputadapter/AccountsController.java
@@ -0,0 +1,19 @@
+package com.lizana.customermicroservice.infrastructure.outputadapter;
+
+import com.lizana.customermicroservice.domain.dto.CustomerDto;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Mono;
+
+@RestController
+@Log4j2
+@RequestMapping("/accountbank")
+public class AccountsController {
+
+  private Mono<CustomerDto> addSignatories(CustomerDto customerDto){
+
+	return null;
+  }
+
+}
